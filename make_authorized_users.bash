6ef1c7435e97381b9c69f008d5b312542709e30c
Michael Soh <sohmc@users.noreply.github.com>
2019-12-22 11:17:57 -0500

    Easier to read

--- make_authorized_users.bash
+++ make_authorized_users.bash
@@ -11,8 +11,13 @@ if [[ -d ./public_keys/ ]]; then
     for k in `ls ./public_keys/*.pub`; do
         echo Adding ${k}
         cat ${k} >> ${AUTH_FILE}
+        echo -e "\n" >> ${AUTH_FILE}
     done
 
+    echo "Removing empty lines"
+    sed '/^$/d' ${AUTH_FILE}
+
+    echo "Chmod 600"
     chmod 600 ${AUTH_FILE}
 fi
 
