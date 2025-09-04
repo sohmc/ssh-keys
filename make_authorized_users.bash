a5bd11cbf5701cefe50470e6fa8cd19a25e5f07c
Michael Soh <2767724+sohmc@users.noreply.github.com>
2025-09-04 16:44:04 -0400

    Moving public_keys to ssh-public_keys

--- make_authorized_users.bash
+++ make_authorized_users.bash
@@ -16,8 +16,8 @@ if [[ -e ${AUTH_FILE} ]]; then
     rm -rf ${AUTH_FILE}
 fi
 
-if [[ -d ./public_keys/ ]]; then
-    for k in `ls ./public_keys/*.pub`; do
+if [[ -d ./ssh-public_keys/ ]]; then
+    for k in `ls ./ssh-public_keys/*.pub`; do
         echo Adding ${k}
         cat ${k} >> ${AUTH_FILE}
         echo -e "\n" >> ${AUTH_FILE}
